0b5b00fea04ad3609a952da4df6ad658c453afee
Hristiyan Hristov <32390803+retry2z@users.noreply.github.com>
2020-09-20 09:46:52 +0300

    -

---
+++ src/code.ts
@@ -0,0 +1,23 @@
+import { Subject } from 'rxjs';
+import { pluck, shareReplay, tap } from 'rxjs/operators';
+
+// simulate url change with subject
+const routeEnd = new Subject();
+
+// grab url and share with subscribers
+const lastUrl = routeEnd.pipe(
+  tap(_ => console.log('executed')),
+  pluck('url'),
+  // defaults to all values so we set it to just keep and replay last one
+  shareReplay(1)
+);
+
+// requires initial subscription
+const initialSubscriber = lastUrl.subscribe(console.log);
+
+// simulate route change
+// logged: 'executed', 'my-path'
+routeEnd.next({data: {}, url: 'my-path'});
+
+// logged: 'my-path'
+const lateSubscriber = lastUrl.subscribe(console.log);
